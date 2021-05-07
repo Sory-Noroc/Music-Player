88b055fc44d616c3a6a67fa1a457c3a0506a959b
Sory <sorinnoroc1@gmail.com>
2021-05-07 21:28:07 +0300

    Fixed slider and playlist indexing

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -32,7 +32,6 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.player.mediaStatusChanged.connect(self.status_changed)
         self.player.stateChanged.connect(self.state_changed)
         self.player.positionChanged.connect(self.position_changed)
-        self.player.volumeChanged.connect(self.volume_changed)
         self.player.setVolume(60)
         self.player.setPlaylist(self.playlist)
         self.state = None  # -1 -> stopped; 0 -> paused; 1 -> playing;
@@ -57,9 +56,7 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.next_button = QtWidgets.QPushButton(self.centralwidget)
         self.previous_button = QtWidgets.QPushButton(self.centralwidget)
         self.volume_slider = QtWidgets.QSlider(self.centralwidget)
-        self.checkbox = QtWidgets.QCheckBox(self.centralwidget)
         self.time_slider = QtWidgets.QSlider(self.centralwidget)
-        self.timer = QtCore.QTimer(self.centralwidget) 
         self.time_label = QtWidgets.QLabel(self.centralwidget)
         self.time_length_label = QtWidgets.QLabel(self.centralwidget)
         self.title = QtWidgets.QLabel(self.centralwidget)
@@ -77,7 +74,7 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.set_box_frame(button_frame)
         self.set_box_frame(timer_frame)
 
-        #  self.play_pause_song is the function that is linked to that button
+        # Linking functions to each button
         self.play_pause_button.clicked.connect(self.play_pause_song)
         self.stop_button.clicked.connect(self.stop_song)
         self.add_new_song_button.clicked.connect(self.add_song)
@@ -100,7 +97,7 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.volume_slider.setValue(100)
         volume_frame.addWidget(self.volume_slider, alignment=QtCore.Qt.AlignLeft)
         self.volume_slider.setOrientation(QtCore.Qt.Horizontal)
-        self.volume_slider.valueChanged.connect(self.volume)
+        self.volume_slider.valueChanged.connect(self.set_volume)
 
         self.time_slider.setMinimum(0)
         self.time_slider.setMaximum(100)
@@ -111,12 +108,6 @@ class UiMainWindow(QtWidgets.QMainWindow):
         timer_frame.addWidget(self.time_label)
         timer_frame.addWidget(self.time_slider)
         timer_frame.addWidget(self.time_length_label)
-
-        self.checkbox.stateChanged.connect(self.auto_play)
-        volume_frame.addWidget(self.checkbox, alignment=QtCore.Qt.AlignRight)
-
-        self.timer.timeout.connect(self.time_hit) 
-        self.timer.start(400)
 
         icon_path = os.path.join('.', 'images/note.png')
         self.icon = QtGui.QIcon(icon_path)
@@ -142,19 +133,21 @@ class UiMainWindow(QtWidgets.QMainWindow):
         list_widget.addItem(item)
 
     def status_changed(self):
+        ''' Signal method triggered when the player status changes ex. noMedia -> Media '''
         pass
 
     def state_changed(self):
+        ''' Signal method triggered when the player state changes ex. playing -> stopped '''
         pass
         
     def position_changed(self, pos):
-        '''This is called when the song time slider is moved'''
-        if self.player.state() == self.player.PlayingState:  # If any song is playing
-            if self.player.isSeekable():  # If any song was added
-                self.time_slider.setValue(pos)
+        '''This is called when the player emits audio'''
+        self.time_slider.setValue(pos)
 
-    def volume_changed(self):
-        pass
+    def add_to_playlist(self, audio_path):
+        ''' Adds the audio with the given path to the playlist '''
+        media = QMediaContent(QtCore.QUrl(audio_path))
+        self.playlist.addMedia(media)
 
     def get_saved_music(self):
         '''Adds the songs that are in the database'''
@@ -163,8 +156,7 @@ class UiMainWindow(QtWidgets.QMainWindow):
             for name, path in audios:
                 self.add_image(self.ui_song_list, name, self.icon)
                 self.audio_paths[name] = path
-                media = list(map(lambda x:QMediaContent(QtCore.QUrl(x)), self.audio_paths.values()))
-                self.playlist.addMedia(media)
+                self.add_to_playlist(path)
         # Updating
         self.audio_widgets = self.ui_song_list.findItems('', QtCore.Qt.MatchContains)
         self.all_audios = list(map(lambda x: x.text(), self.audio_widgets))  # Extracting audio names
@@ -187,12 +179,12 @@ class UiMainWindow(QtWidgets.QMainWindow):
         else:
             if self.player.state() == QMediaPlayer.PlayingState:  # If any sound is playing
                 self.player.pause()
-                self.paused = True
+                self.state = 0
                 self.play_pause_button.setText('Play')  # Button caption
 
             else:
                 self.play_pause_button.setText('Pause')  # Button caption
-                self.paused = False
+                self.state = 1
                 self.player.play()
 
     def stop_song(self):
@@ -203,13 +195,11 @@ class UiMainWindow(QtWidgets.QMainWindow):
 
     def previous_song(self):
         '''Plays the previous song, in the order it was added'''
-
         playlist = self.player.playlist()
         playlist.previous()
 
     def next_song(self):
         '''Plays the next audio'''
-
         playlist = self.player.playlist()
         playlist.next()
 
@@ -239,9 +229,12 @@ class UiMainWindow(QtWidgets.QMainWindow):
             path, ext = os.path.splitext(audio_path)  # taking only the audio path without the extension
             audio_name = os.path.basename(path)                                                                                                                                                                                                                                                   
             self.audio_paths[audio_name] = audio_path
-            self.add_image(self.ui_song_list, audio_name, self.icon)
-            database.insert_in_table((audio_name, audio_path))  # Inserting the audio
-        self.all_songs = self.ui_song_list.findItems('', QtCore.Qt.MatchContains)
+            self.add_to_playlist(audio_path)
+        # Updating
+        self.audio_widgets = self.ui_song_list.findItems('', QtCore.Qt.MatchContains)
+        self.all_audios = list(map(lambda x: x.text(), self.audio_widgets))  # Extracting audio names
+        self.add_image(self.ui_song_list, audio_name, self.icon)
+        database.insert_in_table((audio_name, audio_path))  # Inserting the audio
 
     def time_hit(self):
         '''This tracks and updates the time of the song and it's length'''
@@ -257,36 +250,9 @@ class UiMainWindow(QtWidgets.QMainWindow):
             self.time_slider.setValue(self.player.position()) # update slide bar
 
     def slider_moved(self, pos):
-        '''This is called when the user moves the slider'''                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-        self.player.seek(pos)
-
-    def volume(self, _=None):  # _ is an unused argument that is passed
-        self.player.audio_set_volume(self.volume_slider.value())
-
-    def auto_play(self):
-        '''When it is chosen to play automatically the next song'''
-        if self.checkbox.isChecked():  # If you chose it
-            self.auto_next = True  # This is going to control the checking thread
-            if not self.checking_thread:  # To avoid playing multiple songs at once when spamming next/previous button
-                self.checking_thread = Thread(target=self.check_playing)
-                self.checking_thread.setDaemon(True)  # The thread will finish with the program
-                self.checking_thread.start()
-        else:  # If you chose not to play automatically next song
-            self.auto_next = False  # Kill the thread that checks
-
-    def check_playing(self):
-        '''A thread function that will play the next song when the current one is done'''
-        while self.auto_next:
-            if self.checkbox.isChecked():
-                if self.player.state() == QMediaPlayer.PlayingState or self.paused:
-                    sleep(1)  # To have a slight delay
-                else:
-                    self.next_song()
-                    sleep(2)
-                    self.check_playing()
-            else:
-                self.player.stop()
-                break
+        '''This is called when the user moves the slider'''
+        if self.player.isSeekable():                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            self.player.setPosition(pos)
 
     def default_song(self):
         '''Assigns the first song of the list'''
@@ -296,6 +262,9 @@ class UiMainWindow(QtWidgets.QMainWindow):
             self.play_song(self.all_audios[0])
         except IndexError:
             pass  # Do nothing if buttons are clicked, while there are no songs
+
+    def set_volume(self, pos):
+        self.player.setVolume(pos)
 
     def retranslate_ui(self, main_window):
         '''Setting the text for all the buttons and labels'''
@@ -308,7 +277,6 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.restart_button.setText("Restart")
         self.next_button.setText("Next")
         self.previous_button.setText("Previous")
-        self.checkbox.setText("Autoplay")
         self.time_label.setText('0:00:00')
         self.time_length_label.setText('0:00:00')
         self.title.setText("MP3 Player")
